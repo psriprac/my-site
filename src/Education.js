20f4e984cce81bca7eb6fe0be7b8b9dbe7442cc2
Patrick Sriprachandr <76898394+psriprac@users.noreply.github.com>
2022-01-03 19:43:30 -0800

    Edited Content

--- src/Education.js
+++ src/Education.js
@@ -1,6 +1,13 @@
 const Education = () => (
     <>
-        This is the Education part of the thing.
+        <h5>California State University, San Marcos</h5>
+        <h6>May 2020, Bachelor of Science, Computer Science</h6>
+        <br />
+        <h5>Crawford High School</h5>
+        <h6>2008, Diploma, Honors</h6>
+        <h6>2008, Yearbook Committee, Co-Editor-In-Chief</h6>
+        <h6>2006-2008, Associated Student Body</h6>
+        <h6>2008, Auroras, Senior Class Community Outreach</h6>
     </>
 );
 
--- src/Proficiencies.js
+++ src/Proficiencies.js
@@ -1,6 +1,83 @@
+import Table from "react-bootstrap/Table";
+
+import "./static/css/Proficiencies.css";
+
 const Proficiencies = () => (
     <>
-        I can list all of my skills here.
+        <div className="Skills-List">
+            <ul>
+                <li><h6>Languages</h6></li>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>C++</li>
+                    <li>Python</li>
+                </ul>
+                <br />
+                <li><h6>IT Solutions</h6></li>
+                <ul>
+                    <li>Active Directory</li>
+                    <li>VMware vSphere</li>
+                    <li>VMware Horizon</li>
+                    <li>VMware App Volumes</li>
+                    <li>HCL BigFix</li>
+                    <li>PowerShell</li>
+                    <li>ServiceNow IT Service Management</li>
+                    <li>ServiceNow Hardware Asset Management</li>
+                </ul>
+                <br />
+                <li><h6>Administrative</h6></li>
+                <ul>
+                    <li>Leadership</li>
+                    <li>Interpersonal Communication</li>
+                    <li>Teamwork</li>
+                    <li>Organization</li>
+                </ul>
+            </ul>
+            </div>
+            <div className="Skills-Table">
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>Languages</th>
+                        <th>IT Solutions</th>
+                        <th>Administrative</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>C++</li>
+                            <li>Python</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Active Directory</li>
+                            <li>VMware vSphere</li>
+                            <li>VMware Horizon</li>
+                            <li>VMware App Volumes</li>
+                            <li>HCL BigFix</li>
+                            <li>PowerShell</li>
+                            <li>ServiceNow IT Service Management</li>
+                            <li>ServiceNow Hardware Asset Management</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Leadership</li>
+                            <li>Interpersonal Communication</li>
+                            <li>Teamwork</li>
+                            <li>Organization</li>
+                        </ul>
+                    </td>
+                </tbody>
+            </Table>
+        </div>
     </>
 )
 
